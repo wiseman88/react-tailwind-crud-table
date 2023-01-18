9314ee32ed5bb382a7eefcef5cd93709e236085b
wiseman88 <wiseadam02@gmail.com>
2023-01-18 06:54:28 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit harum
+        nemo tempore ratione provident, saepe temporibus amet iste perspiciatis
+        dolor?
+      </h1>
+    </div>
+  );
+}
+
+export default App;
