38ccf5482b786c0517e956fa7f210c1482ac48a6
wiseman88 <wiseadam02@gmail.com>
2023-02-13 15:03:11 +0100

    refactor modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import Header from "./components/Header";
-import { useSelector } from "react-redux";
-import Modal from "./components/Modal";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
@@ -12,8 +10,6 @@ function App() {
     body: "h-screen bg-gray-900 text-white",
   };
 
-  const showModal = useSelector((state) => state.modal.showModal);
-
   return (
     <BrowserRouter>
       <div className={classes.body}>
@@ -23,7 +19,6 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Routes>
-        {showModal && <Modal />}
       </div>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Form from "../components/product/Form";
 import Table from "../components/product/table/Table";
+import { useSelector } from "react-redux";
+import Modal from "../components/Modal";
 
 export const Home = () => {
+  const showModal = useSelector((state) => state.modal.showModal);
   return (
     <>
       <Form />
       <Table />
+      {showModal && <Modal />}
     </>
   );
 };
