b7442c41e2f0b9a9b010757b24db150f2cea455b
wiseman88 <wiseadam02@gmail.com>
2023-02-19 17:07:27 +0100

    refactor: app css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import "./App.css";
 import Header from "./components/Header";
+import styles from "./styles/styles.module.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
 import { About } from "./pages/About";
 
 function App() {
-  const classes = {
-    body: "h-screen bg-gray-900 text-white",
-  };
-
   return (
     <BrowserRouter>
-      <div className={classes.body}>
+      <div className={styles.body}>
         <Header />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-        </Routes>
+        <main className="py-10">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Routes>
+        </main>
       </div>
     </BrowserRouter>
   );
